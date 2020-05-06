313cfa5b3277614ce18b858a23b196bf5ddf944b
WILLIAM SANCHES LIMA <WSLIMA@CORP.AD.EMB>
2020-05-06 02:50:53 -0300

    eda das entidades e pivot table pivot = pd.pivot_table(df,index=["pessoa_municipio_uf_sigla","tipoSancao_descricaoResumida"], values=["id"],aggfunc={"id":len}) utl.print_all_df(pivot)

--- eda.py
+++ eda.py
@@ -9,23 +9,26 @@ data = utl.getListFromMongoCol('ceis', 'register')
 df = utl.flattenListAsDF(data)
 
 # analises basicas no dataframe
-'''
-utl.printwhf(df.head, 'df.head')
-utl.printwhf(df.describe, 'df.describe')
-utl.printwhf(df.pessoa_tipoPessoa.value_counts(), 'df.pessoa_tipoPessoa.value_counts()')
-utl.printwhf(df.pessoa_municipio_uf_nome.value_counts(), 'df.pessoa_municipio_uf_nome.value_counts()')
-utl.printwhf(df.pessoa_cnae_secao.value_counts(), 'df.pessoa_cnae_secao.value_counts()')
-utl.printwhf(df.orgaoSancionador_nome.value_counts(), 'df.orgaoSancionador_nome.value_counts()')
-utl.printwhf(df.orgaoSancionador_siglaUf.value_counts(), 'df.orgaoSancionador_siglaUf.value_counts()')
-utl.printwhf(df.orgaoSancionador_poder.value_counts(), 'df.orgaoSancionador_poder.value_counts()')
-#'''
+#>>> utl.printwhf(df.head, 'df.head')
+#>>> utl.printwhf(df.describe, 'df.describe')
+#>>> utl.printwhf(df.pessoa_tipoPessoa.value_counts(), 'df.pessoa_tipoPessoa.value_counts()')
+#>>> utl.printwhf(df.pessoa_municipio_uf_sigla.value_counts(), 'df.pessoa_municipio_uf_sigla.value_counts()')
+#>>> utl.printwhf(df.pessoa_cnae_secao.value_counts(), 'df.pessoa_cnae_secao.value_counts()')
+#>>> utl.printwhf(df.orgaoSancionador_nome.value_counts(), 'df.orgaoSancionador_nome.value_counts()')
+#>>> utl.printwhf(df.orgaoSancionador_siglaUf.value_counts(), 'df.orgaoSancionador_siglaUf.value_counts()')
+#>>> utl.printwhf(df.orgaoSancionador_poder.value_counts(), 'df.orgaoSancionador_poder.value_counts()')
 
-# identifiquei estados com siglas minusculas => padronizando todos para maiusculas
+# identifiquei estados com siglas minusculas na serie orgaoSancionador_siglaUf => padronizando todos para maiusculas
 df['orgaoSancionador_siglaUf'] = df['orgaoSancionador_siglaUf'].str.upper()
 # identifiquei estados vazios => trocando para valor nao numerico => https://pandas.pydata.org/pandas-docs/stable/user_guide/missing_data.html
 df['orgaoSancionador_siglaUf'].replace([''], np.nan, inplace=True)
 # contagem atualizada apos correcao
 #>>> print(df.orgaoSancionador_siglaUf.value_counts())
+
+# identifiquei um valor nao valido na serie pessoa_municipio_uf_sigla => trocando para valor nao numerico
+df['pessoa_municipio_uf_sigla'].replace(['-1'], np.nan, inplace=True)
+# contagem atualizada apos correcao
+#>>> print(df.pessoa_municipio_uf_sigla.value_counts())
 
 # convertendo series dataInicioSancao de string para dateTime
 dataInicioSancao = pd.to_datetime(df['dataInicioSancao'])
@@ -126,19 +129,11 @@ for tipoSancao in df_tipoSancao:
     df_tipoSancao_it = df[df_tipoSancao_boolean]
     prazo_tipoSancao = (df_tipoSancao_it["dataFimSancao"] - df_tipoSancao_it["dataInicioSancao"])
     prazoSancao_tipoSancao.append(prazo_tipoSancao.describe())
-    print(tipoSancao)
-    print(prazo_tipoSancao.describe())
-    print()
+    #>>> print(tipoSancao)
+    #>>> print(prazo_tipoSancao.describe())
+    #>>> print()
 #for item in prazoSancao_tipoSancao:
 #    print(item[1])
-
-
-print()
-print()
-print()
-print()
-print()
-
 
 # analisando prazos das sancoes por estado
 df_uf = df['pessoa_municipio_uf_nome'].unique()
@@ -149,8 +144,58 @@ for uf in df_uf:
     df_uf_it = df[df_uf_boolean]
     prazo_uf = (df_uf_it["dataFimSancao"] - df_uf_it["dataInicioSancao"])
     prazoSancao_uf.append(prazo_uf.describe())
-    print(uf)
-    print(prazo_uf.describe())
-    print()
+    #>>> print(uf)
+    #>>> print(prazo_uf.describe())
+    #>>> print()
 #for item in prazoSancao_uf:
 #    print(item[1])
+
+# a analise de prazos nao ofereceu nenhum insight visto que os valores de desvios padroes sao muito altos
+
+# vamos, entao, efetuar analises pelo prisma das entidades sancionadas
+# vamos verificar o indice de reincidencia de sancoes por entidades
+pessoaCodigo = df['pessoa_codigoFormatado']
+pessoaCodigo_value_counts = pessoaCodigo.value_counts()
+#>>> print(pessoaCodigo_value_counts[pessoaCodigo_value_counts > 2].sum() / pessoaCodigo_value_counts.sum())
+# o indice de reincidencia e de cerca de 16,3 %, uma taxa razoavel que vale a pena analisarmos
+
+# filtrando o dataframe para obter apenas pessoas com quantidade de sancoes > 2
+pessoaReincidente = pessoaCodigo_value_counts[pessoaCodigo_value_counts > 2]
+df_pessoaReincidente = df[df.pessoa_codigoFormatado.isin(pessoaReincidente.index)]
+#>>> print(df_pessoaReincidente)
+
+# quais os tipos (PF ou PJ?)
+#>>> print(df_pessoaReincidente.pessoa_tipoCodigo.value_counts())
+#>>> df_pessoaReincidente_value_counts = df_pessoaReincidente.pessoa_tipoCodigo.value_counts()
+#>>> labels = ['Pessoa Física', 'Pessoa Jurídica', 'Não informado']
+#>>> sizes = df_pessoaReincidente_value_counts.values
+#>>> fig1, ax1 = plt.subplots()
+#>>> ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+#>>> ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+#>>> plt.show()
+
+# analisar onde se encontram as entidades com maior indice de reincidencia
+#>>> print(df_pessoaReincidente.pessoa_municipio_uf_sigla.value_counts())
+
+# agora vamos verificar se elas reincidem nas mesmas sancoes
+#>>> print(df_pessoaReincidente.tipoSancao_descricaoResumida.value_counts())
+
+# tempo de vida das empresas reincidentes
+df_pessoaReincidente_pj = df_pessoaReincidente[df_pessoaReincidente.pessoa_tipoCodigo == 'CNPJ']
+pessoaReincidentePJ_dataAbertura = pd.to_datetime(df_pessoaReincidente_pj['pessoa_dataAbertura'], errors='coerce')
+pessoaReincidentePJ_tempoVida = pd.Timestamp.today() - pessoaReincidentePJ_dataAbertura
+#>>> print(pessoaReincidentePJ_tempoVida.describe())
+
+# comparando com o tempo de vida das empresas nao reincidentes
+pessoaNaoReincidente = pessoaCodigo_value_counts[pessoaCodigo_value_counts == 1]
+df_pessoaNaoReincidente = df[df.pessoa_codigoFormatado.isin(pessoaNaoReincidente.index)]
+df_pessoaNaoReincidente_pj = df_pessoaNaoReincidente[df_pessoaNaoReincidente.pessoa_tipoCodigo == 'CNPJ']
+pessoaNaoReincidentePJ_dataAbertura = pd.to_datetime(df_pessoaNaoReincidente_pj['pessoa_dataAbertura'], errors='coerce')
+pessoaNaoReincidentePJ_tempoVida = pd.Timestamp.today() - pessoaNaoReincidentePJ_dataAbertura
+#>>> print(pessoaNaoReincidentePJ_tempoVida.describe())
+
+
+pivot = pd.pivot_table(df,index=["pessoa_municipio_uf_sigla","tipoSancao_descricaoResumida"], values=["id"],aggfunc={"id":len})
+#new_order= ["id"]
+#pivot = pivot.reindex(new_order, axis=1)
+utl.print_all_df(pivot)
